afc54f7345c4279147fc33438d37d27ed6abed8c
Ivan Kandikov <ikandikov@routeone.com>
2018-06-02 15:31:05 -0400

    Refactor: DateTimeZone

--- src/test/java/org/kandikov/stats/TransactionControllerTest.java
+++ src/test/java/org/kandikov/stats/TransactionControllerTest.java
@@ -1,7 +1,6 @@
 package org.kandikov.stats;
 
 import org.joda.time.DateTime;
-import org.joda.time.DateTimeZone;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,7 +11,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import static org.joda.time.DateTimeZone.*;
+import static org.joda.time.DateTimeZone.UTC;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
